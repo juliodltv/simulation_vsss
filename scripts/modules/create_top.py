32287670e9a1e76616d2a124e8b0287db04fe1bf
Xpsp <juliodltv@gmail.com>
2023-09-13 20:26:41 -0600

    new tags

--- scripts/modules/create_top.py
+++ scripts/modules/create_top.py
@@ -3,21 +3,55 @@
 import sys, rospkg, cv2, numpy as np
 sys.dont_write_bytecode = True
 
-offset = 50
-colors = {'green': 1, 'pink': [0,2], 'red': 2}
+colors = {'green': 1, 'pink': [0,2], 'red': 2, 'cyan': [0,1]}
 
-top_blue = np.zeros((897, 897, 3), dtype=np.uint8)
-top_yellow = np.zeros((897, 897, 3), dtype=np.uint8)
+top_blue = np.zeros((800, 800, 3), dtype=np.uint8)
+top_yellow = np.zeros((800, 800, 3), dtype=np.uint8)
 
-top_blue[897//2:897-offset,offset:897-offset,0] = 255 # Blue
-top_yellow[897//2:897-offset,offset:897-offset,1:] = 255 # Yellow
+top_blue[75+300+50:800-75,75:800-75,0] = 255 # Blue
+top_yellow[75+300+50:800-75,75:800-75,1:] = 255 # Yellow
 
-for i, value in enumerate(colors.values()):
-    top_blue[offset:897//2,offset:897-offset,value] = 255
-    top_yellow[offset:897//2,offset:897-offset,value] = 255
+# green-red, cyan-red, red-green, cyan-green, pink-green, red-cyan, green-cyan, pink-cyan, green-pink, cyan-pink
 
+combinations = [
+    
+    # Consider cyan as default color
+    # cyan at right
+    [colors['green'], colors['cyan']],
+    [colors['pink'], colors['cyan']],
+    [colors['red'], colors['cyan']],
+    
+    # # cyan at left
+    # [colors['cyan'], colors['green']],
+    # [colors['cyan'], colors['pink']],
+    # [colors['cyan'], colors['red']],
+    
+    # # Consider green as default color
+    # # green at right
+    # [colors['cyan'], colors['green']],
+    # [colors['pink'], colors['green']],
+    # [colors['red'], colors['green']],
+    
+    # # green at left
+    # [colors['green'], colors['cyan']],
+    # [colors['green'], colors['pink']],
+    # [colors['green'], colors['red']],
+    
+]
+
+for i, combination in enumerate(combinations):
+    print(combination)
+    top_blue[75:75+300,75:75+300,combination[0]] = 255
+    top_blue[75:75+300,75+300+50:800-75,combination[1]] = 255
+    
+    top_yellow[75:75+300,75:75+300,combination[0]] = 255
+    top_yellow[75:75+300,75+300+50:800-75,combination[1]] = 255
+    
     cv2.imwrite(rospkg.RosPack().get_path('simulation_vsss')+f'/media/materials/textures/blue_{i}.png', top_blue)
     cv2.imwrite(rospkg.RosPack().get_path('simulation_vsss')+f'/media/materials/textures/yellow_{i}.png', top_yellow)
     
-    top_blue[offset:897//2,offset:897-offset,value] = 0
-    top_yellow[offset:897//2,offset:897-offset,value] = 0
+    top_blue[75:75+300,75:75+300,combination[0]] = 0
+    top_blue[75:75+300,75+300+50:800-75,combination[1]] = 0
+    
+    top_yellow[75:75+300,75:75+300,combination[0]] = 0
+    top_yellow[75:75+300,75+300+50:800-75,combination[1]] = 0
